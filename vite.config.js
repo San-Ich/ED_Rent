1100a66210c603938fa21b5572893dc5ba990ac3
Anfariza <priza12324@gmail.com>
2026-06-23 10:54:41 +0700

    memperbaiki file CSS

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/landing-layout.css",
+                "resources/css/catalog.css",
+                "resources/css/detail-profile.css",
+                "resources/css/detail-struk.css",
+                "resources/css/icon-admin.css",
+                "resources/css/list-rental.css",
+                "resources/css/payment-failed.css",
+                "resources/css/payment.css",
+            ],
             refresh: true,
         }),
     ],
